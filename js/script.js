a27f04c8add26d1a48fa781f14b81dce33fe975b
yume0222 <134505601+yume0222@users.noreply.github.com>
2025-05-09 17:08:28 +0900

    feat: 画像ギャラリーの実装

--- js/script.js
+++ js/script.js
@@ -45,3 +45,14 @@ window.addEventListener('load', () => {
     }
   )
 });
+
+// 画像ギャラリー
+const mainImage = document.querySelector('.gallery-image img');
+const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
+thumbImages.forEach((thumbImage) => {
+  thumbImage.addEventListener('mouseover', (event) => {
+    mainImage.src = event.target.src;
+    mainImage.animate({opacity: [0, 1]}, 500);
+  });
+  console.log(thumbImage);
+});
