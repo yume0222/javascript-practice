b85f9921da1ed1157ed6008758d8c461d5b4bdfe
yume0222 <134505601+yume0222@users.noreply.github.com>
2025-05-11 18:11:50 +0900

    feat: スライドメニュー、スクロールで画面を表示

--- js/script.js
+++ js/script.js
@@ -54,5 +54,70 @@ thumbImages.forEach((thumbImage) => {
     mainImage.src = event.target.src;
     mainImage.animate({opacity: [0, 1]}, 500);
   });
-  console.log(thumbImage);
+});
+
+// スライドメニュー
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuItems = document.querySelectorAll('.menu-list li')
+const menuOptions = {
+  duration: 1400,
+  easing: 'ease',
+  fill: 'forwards',
+};
+// メニューを開く
+menuOpen.addEventListener('click', () => {
+  menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+  // リストを1つずつ順に表示
+  menuItems.forEach((menuItem, index) => {
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        translate: ['2rem', 0],
+      },
+      {
+        duration: 2400,
+        easing: 'ease',
+        delay: index * 300,
+        fill: 'forwards',
+      }
+    );
+  });
+});
+// メニューを閉じる
+menuClose.addEventListener('click', () => {
+  menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+  menuItems.forEach((menuItem, index) => {
+    menuItem.animate({opacity: [1, 0]}, menuOptions);
+  });
+});
+
+// スクロールで画面を表示
+// 監視対象が範囲内に現れたら実行する動作
+const animateFadein = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+      {
+        opacity: [0, 1],
+        filter: ['blur(.4rem)', 'blur(0)'],
+        translate: ['0 4rem', 0],
+      },
+      {
+        duration: 2000,
+        easing: 'ease',
+        fill: 'forwards',
+      });
+      // 1度ふわっとされたら監視をやめる
+      obs.unobserve(entry.target);
+    }
+  });
+};
+// 監視ロボットの設定
+const fadeObserver = new IntersectionObserver(animateFadein);
+// 監視するように指示
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
 });
